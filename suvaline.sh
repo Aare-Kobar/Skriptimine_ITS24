643543c5cfbcda5dcdc0544fd0897c4a67e94ffa
Aare-Kobar <aareaigar@hkhk.edu.ee>
2025-09-19 10:47:54 +0300

    Minu esimene Git'i projekt käsurealt

---
+++ suvaline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#See on minu esimene bash'i skript, mis teeb midagi.
+
+TERVITUS="See on tervitus AI poolt" 
+
+
+echo $TERVITUS
+echo "Sina oled $(whoami)"
+echo "Tore oli sinuga tuttavaks saada!"
